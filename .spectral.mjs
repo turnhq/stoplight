e50f663b36a87e8d3c49ec152a5ed45873006859
Jose Vazquez <vaudelaire@gmail.com>
2023-10-04 11:09:23 -0600

    Stoplight metadata files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjcyNTE0/spectral.js?branch_slug=master&token=3be48115-efc9-4e91-b8e7-60011fbc42dc";
+export default { extends: ruleset };
